d23f37e8d438422b35f337601f54cbb94da15618
Dan Mauk <dkm8923@gmail.com>
2026-03-06 15:20:30 -0500

    Added 'ApplicationId' validation to validator files

--- Services/Security/Logic.Security/Validators/Permission/InsertUpdatePermissionRequestValidator.cs
+++ Services/Security/Logic.Security/Validators/Permission/InsertUpdatePermissionRequestValidator.cs
@@ -11,7 +11,7 @@ public class InsertUpdatePermissionRequestValidator : AbstractValidator<InsertUp
         public const string Name = "Name";
         public const string Description = "Description";
         public const string CurrentUser = "CurrentUser";
-
+        public const string ApplicationId = "ApplicationId";
     }
 
     public InsertUpdatePermissionRequestValidator()
@@ -26,6 +26,9 @@ public class InsertUpdatePermissionRequestValidator : AbstractValidator<InsertUp
         RuleFor(v => v.Description)
             .Length(0, 256).WithMessage(ValidatorUtilities.CreateMaxLengthErrorMessage(EntityFieldNames.Description, 256));
 
+        RuleFor(v => v.ApplicationId)
+            .GreaterThan(0).WithMessage(ValidatorUtilities.CreateRequiredFieldErrorMessage(EntityFieldNames.ApplicationId));
+        
         RuleFor(v => v.CurrentUser)
             .NotEmpty().WithMessage(ValidatorUtilities.CreateRequiredFieldErrorMessage(EntityFieldNames.CurrentUser))
             .Length(1, 64).WithMessage(ValidatorUtilities.CreateMaxLengthErrorMessage(EntityFieldNames.CurrentUser, 64));
--- Services/Security/Logic.Security/Validators/Role/InsertUpdateRoleRequestValidator.cs
+++ Services/Security/Logic.Security/Validators/Role/InsertUpdateRoleRequestValidator.cs
@@ -11,6 +11,7 @@ public class InsertUpdateRoleRequestValidator : AbstractValidator<InsertUpdateRo
         public const string Name = "Name";
         public const string Description = "Description";
         public const string CurrentUser = "CurrentUser";
+        public const string ApplicationId = "ApplicationId";
 
     }
 
@@ -26,6 +27,9 @@ public class InsertUpdateRoleRequestValidator : AbstractValidator<InsertUpdateRo
         RuleFor(v => v.Description)
             .Length(0, 256).WithMessage(ValidatorUtilities.CreateMaxLengthErrorMessage(EntityFieldNames.Description, 256));
 
+        RuleFor(v => v.ApplicationId)
+            .GreaterThan(0).WithMessage(ValidatorUtilities.CreateRequiredFieldErrorMessage(EntityFieldNames.ApplicationId));
+            
         RuleFor(v => v.CurrentUser)
             .NotEmpty().WithMessage(ValidatorUtilities.CreateRequiredFieldErrorMessage(EntityFieldNames.CurrentUser))
             .Length(1, 64).WithMessage(ValidatorUtilities.CreateMaxLengthErrorMessage(EntityFieldNames.CurrentUser, 64));
