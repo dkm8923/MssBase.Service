6ee2ed43cbdd785d2e8e38f697657a5cf152e803
Dan Mauk <dkm8923@gmail.com>
2026-03-17 14:02:46 -0400

    Removed explicit referecne to IncludeRelated in IRolePermissionService

--- Services/Security/Contract.Security/RolePermission/IRolePermissionService.cs
+++ Services/Security/Contract.Security/RolePermission/IRolePermissionService.cs
@@ -6,8 +6,8 @@ namespace Contract.Security.RolePermission;
 
 public interface IRolePermissionService
 {
-    public Task<ErrorValidationResult<IEnumerable<RolePermissionDto>>> GetAll(BaseServiceGet req, bool includeRelated = false);
-    public Task<ErrorValidationResult<RolePermissionDto>> GetById(int roleId, BaseServiceGet req, bool includeRelated = false);
+    public Task<ErrorValidationResult<IEnumerable<RolePermissionDto>>> GetAll(BaseServiceGet req);
+    public Task<ErrorValidationResult<RolePermissionDto>> GetById(int roleId, BaseServiceGet req);
     public Task<ErrorValidationResult<IEnumerable<RolePermissionDto>>> Filter(FilterRolePermissionServiceRequest req);
     public Task<ErrorValidationResult<RolePermissionDto>> Insert(InsertUpdateRolePermissionRequest req);
     public Task<ErrorValidationResult<RolePermissionDto>> Update(int roleId, InsertUpdateRolePermissionRequest req);
